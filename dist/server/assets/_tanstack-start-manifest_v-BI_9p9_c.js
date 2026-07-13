c9c672644251ed4f8c618117e9de96fc928745a6
shelkeaditya <shelkeaditya69@gmail.com>
2026-07-13 22:26:47 +0530

    Fix .gitignore, stop tracking build artifacts

--- dist/server/assets/_tanstack-start-manifest_v-BI_9p9_c.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CKQG3V1D.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CKQG3V1D.js" } }] }, "/": { filePath: "C:/Portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-olN3hju1.js"] } } });
-export {
-  tsrStartManifest
-};
